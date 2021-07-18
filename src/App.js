6d0da267af60c4bd06ecde18fe78e6eab0a39430
DoanNT <nguyentiendoanhn09@gmail.com>
2021-07-18 07:19:46 +0700

    update menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css'
 import Menu from './Component/Menu/Menu';
 import StatusForm from './Component/StatusForm/StatusForm';
 import StatusList from './Component/StatusList/StatusList';
+import {
+  BrowserRouter as Router,
+
+} from "react-router-dom";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,31 +17,34 @@ class App extends Component {
     }
   }
   
-  
   render() {  
     var tasks=this.state;
     return (
-      <div className="container">
-        <Menu/>
-  <div className="row">
-    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-      <StatusForm/>
-    </div>
-    <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-      <button type="button" className="btn btn-primary mb-15" >
-        <span className="fa fa-plus mr-5 " />Thêm Công Việc
-      </button>
-     
-      <div className="row mt-15">
-        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <StatusList tasks={tasks}/>
+      <Router>
+        <div className="container">
+          <Menu />
+          <div className="row">
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              <Switch>
+                <Route exact path="/addstatus">
+                  <StatusForm />
+                </Route>
+              </Switch>
+            </div>
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <div className="row mt-15">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <Switch>
+                    <Route path="/managerstatus">
+                      <StatusList tasks={tasks} />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-    
+      </Router>
     );
   }
 }
--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from "react-router-dom";
 
+function MyLink({ label, to, activeOnlyWhenExact }) {
+  let match = useRouteMatch({
+    path: to,
+    exact: activeOnlyWhenExact
+  });
+  return (
+    <Link className={match ? "menu-item active" : "menu-item"} to={to}>{label} </Link>
+  );
+}
 class Menu extends Component {
     render() {
         return (
+           
             <nav className="navbar navbar-default" role="navigation">
 
             <div className="navbar-header">
@@ -10,12 +27,13 @@ class Menu extends Component {
             </div>
   
             <div className="collapse navbar-collapse navbar-ex1-collapse">
-              <ul className="nav navbar-nav">
-                <li className="active"><a >Trang chủ </a></li>
-                <li className><a >Quản lý Trạng Thái </a></li>
+              <ul className="nav navbar-nav  ">
+                <li ><MyLink to='/addstatus' label='Thêm Trạng Thái'></MyLink></li>
+                <li ><MyLink to='/managerstatus' label='Quản lý Trạng Thái'></MyLink></li>
               </ul>
             </div>
           </nav>
+         
         );
     }
 }
